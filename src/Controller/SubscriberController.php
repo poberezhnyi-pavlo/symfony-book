48b16ffb67c5ba0dafb76b21a1aaab014ac7283d
Pavlo Poberezhnyi <poberezhnyi_pp@groupbwt.com>
2023-03-24 14:03:49 +0200

    fixed swagger

--- src/Controller/SubscriberController.php
+++ src/Controller/SubscriberController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Attribute\RequestBody;
+use App\Model\ErrorResponse;
 use App\Model\SubscriberRequest;
 use App\Service\SubscriberService;
 use Nelmio\ApiDocBundle\Annotation\Model;
@@ -12,7 +13,8 @@ use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class SubscriberController extends AbstractController
+
+final class SubscriberController extends AbstractController
 {
     public function __construct(private readonly SubscriberService $subscriberService)
     {
@@ -22,9 +24,15 @@ class SubscriberController extends AbstractController
      * @OA\Response(
      *     response=200,
      *     description="Subscribe email to newsletter mailling list",
-     *
-     *     @Model(type=MyClass::class)
      * )
+     * @OA\Response(
+     *     response="400",
+     *     description="Validation failed",
+     *
+     *     @Model(type=ErrorResponse::class)
+     * )
+     *
+     * @OA\RequestBody(@Model(type=SubscriberRequest::class))
      */
     #[Route(path: '/api/v1/subscribe', methods: 'POST')]
     public function subscribe(#[RequestBody] SubscriberRequest $subscriberRequest): Response
--- src/Model/ErrorResponse.php
+++ src/Model/ErrorResponse.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace App\Model;
 
+use OpenApi\Annotations as OA;
+
 class ErrorResponse
 {
     public function __construct(private readonly string $message, private readonly mixed $details = null)
     {
     }
 
+    /**
+     * @OA\Property(type="object")
+     */
     public function getDetails(): mixed
     {
         return $this->details;
